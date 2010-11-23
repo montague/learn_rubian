7b952b6c0df42864e810363415ab1d97f2583b32
Ian Asaff <ian.asaff@gmail.com>
2010-11-22 20:40:19 -0500

    show_methods

--- tests.rb
+++ tests.rb
@@ -1,8 +1,12 @@
 module Learn_Rubian
   class Tests
     class << self
+      #show all methods defined for an object
       def show_methods(o)
-        o.methods.select {|m| o.method(m).receiver.to_s[Regexp.new("^#{o.to_s}$")]}
+        o.methods.reject do |m|
+          c = o.is_a?(Class) ? o : o.class
+          c.superclass.methods.include?(m)
+        end
       end
     end
   end
